215bbc23500879e774e824e12dc12bf329060e80
tarkavs <tarkavsunday@outlook.com>
2023-05-01 06:14:19 -0700

    the last updat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
-from flask import jsonify
-
+"""
+landing page for api
+"""
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status')
-def get_status():
-    '''Gets the status of the API.
-    '''
-    return jsonify(status='OK')
+def app_status():
+    """
+    Simply returns the state of the api.
+    """
+    return(jsonify(status="OK"))
 
 
 @app_views.route('/stats')
-def get_stats():
-    '''Gets the number of objects for each type.
-    '''
-    objects = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
-    }
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
+def app_get_count():
+    """
+    Returns statistics about the number of objects available
+    """
+    tojson = {}
+    for cls in storage.available_classes:
+        string = str(cls).lower()
+        if string[-1] is 'y':
+            string = string[0:-1] + "ies"
+        else:
+            string += "s"
+        tojson.update({string: storage.count(cls)})
+    return(jsonify(tojson))
